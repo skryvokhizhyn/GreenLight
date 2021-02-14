81d590180cf77b5f55c17a4b33361201ff712c3a
skryv1 <skryvokhizhyn@gmail.com>
2021-02-14 16:12:47 +0200

    Removed legacy Miner code

--- GreenLightMiner/GreenLightMiner/PathBuilder.cpp
+++
@@ -1,8 +0,0 @@
-#include "PathBuilder.h"
-
-using namespace glm;
-
-PathBuilderResult glm::GeneratePath(const Point3dVector & points, double tolerance, uint16_t minSeriesLength)
-{
-	return PathBuilderResult();
-}
